d154d9b7d02ebfdeaad9ed489748a91c1ad28819
sbhusal <sbhusal@marvell.com>
2024-06-20 16:28:40 -0600

    Changes to bf class

--- storage/bloomfilter.py
+++ storage/bloomfilter.py
@@ -1,6 +1,7 @@
 from typing import Any
 import mmh3
 
+
 class BloomFilter:
     def __init__(self) -> None:
         # calculations from https://hur.st/bloomfilter/?n=4000&p=0.000001&m=&k=5
@@ -10,19 +11,20 @@ class BloomFilter:
         # The 5 hash functions.
         self.hash_seeds = [52711, 648391, 9737333, 3713921, 1744400]
 
-    def insert(self, input: Any) -> None:
+    def get_bit_pattern(self, input: Any) -> int:
         set_bits = 0
         for seed in self.hash_seeds:
             hash_value = mmh3.hash(input, seed=seed)
             set_bits |= 1 << (hash_value % self.size)
-        self.filter |= set_bits
+        return set_bits
+
+    def insert(self, input: Any) -> None:
+        self.filter |= self.get_bit_pattern(input)
 
     def check(self, input: Any) -> bool:
-        set_bits = 0
-        for seed in self.hash_seeds:
-            hash_value = mmh3.hash(input, seed=seed)
-            set_bits |= 1 << (hash_value % self.size)
-        return (set_bits & self.filter) == set_bits
+        check = self.get_bit_pattern(input)
+        return (check & self.filter) == check
+
 
 if __name__ == "__main__":
     bf = BloomFilter()
